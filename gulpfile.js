efe102191c83743651b96f82a628fd35f8dadc91
Antonio0402 <21210003@ms.uit.edu.vn>
2023-08-17 16:48:28 +0700

    update browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const { src, dest, watch, series } = require('gulp');
+const browsersync = require('browser-sync').create();
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
@@ -7,17 +8,34 @@ const postcss = require('gulp-postcss');
 /* Sass Task */
 function scssTask() {
   return src('./scss/main.scss')
-  .pipe(sass().on('error', sass.logError))
-  .pipe(postcss([autoprefixer , cssnano()]))
-  .pipe(dest('./dist/css', {sourcemaps: '.'}))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([autoprefixer, cssnano()]))
+    .pipe(dest('./dist/css', { sourcemaps: '.' }))
+}
+
+/* Browsersync Task */
+function browsersyncServe(cb) {
+  browsersync.init({
+    server: {
+      baseDir: '.'
+    }
+  });
+  cb();
+}
+
+/* Browsersync reload when files saved */
+function browsersyncReload(cb) {
+  browsersync.reload();
+  cb();
 }
 
 /* Watch Task */
 function watchTask() {
-  watch(['./scss/**/*.scss', '*.html'], scssTask);
+  watch(['./scss/**/*.scss', '*.html'], series(scssTask, browsersyncReload));
 }
 
 exports.default = series(
   scssTask,
+  browsersyncServe,
   watchTask
 );
