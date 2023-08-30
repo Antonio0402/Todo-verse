571bb258f2b6fa9ab7451df9c3114480fec9987a
Antonio0402 <21210003@ms.uit.edu.vn>
2023-08-30 19:50:51 +0700

    fix cookie not send

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -7,8 +7,7 @@ export const login = async (user_email: string, password: string) => {
   return user_email || password
     ? await fetch(`${API_AUTH_URL}/login`, {
       method: "POST",
-      credentials: "include",
-      mode: "cors",
+      credentials: "same-origin",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json"
